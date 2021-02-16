ae214ac3c50e20ebc2c9c1653beb2fde1ce44a3a
Emmanuel Adedeji <emmans4destiny@gmail.com>
2021-02-17 01:31:46 +0200

    User routes done

--- src/middlewares/authorize.js
+++ src/middlewares/authorize.js
@@ -1,41 +1,44 @@
-const jwt = require("jsonwebtoken");
-const UserModel = require("../userModel/Schema");
-const { verifyJWT } = require("../auth/authTools");
+const { verifyJWT } = require("../auth/authTools")
+const db = require("../db/index")
 
 const authorize = async (req, res, next) => {
   try {
-    const token = req.cookies.accessToken;
-    const decoded = await verifyJWT(token);
-    const user = await UserModel.findOne({
-      _id: decoded._id,
-    });
-    console.log("I am the user",user);
+    const token = req.cookies.accessToken
+   
+    console.log(token)
+    const decoded = await verifyJWT(token)
+    const user = await db.query('SELECT * FROM "users" WHERE _id= $1',
+      [decoded._id])
 
     if (!user) {
-      throw new Error();
+      throw new Error()
     }
 
-    req.token = token;
-    req.user = user;
-    next();
+    req.token = token
+    req.user = user.rows[0]
+
+    if (req.user.refresh_token === null) {
+      const err = new Error("Sorry you already logged out!")
+      err.httpStatusCode = 403
+      next(err)
+    }
+    next()
   } catch (e) {
-    //console.log(e)
-    const err = new Error("Please authenticate");
-    err.httpStatusCode = 401;
-    next(err);
+    const err = new Error("Please authenticate")
+    err.httpStatusCode = 401
+    next(err)
   }
-};
+}
 
-const adminOnlyMiddleware = async (req, res, next) => {
-  if (req.user && req.user.role === "admin") next();
+const onlyForAdmin = async (req, res, next) => {
+  if (req.user && req.user.title === "admin") next()
   else {
-    const err = new Error("Only for admins!");
-    err.httpStatusCode = 403;
-    next(err);
+    const err = new Error("Only for admin!")
+    err.httpStatusCode = 403
+    next(err)
   }
-};
+}
 
-module.exports = {
-  authorize,
-  adminOnlyMiddleware,
-};
+
+
+module.exports = { authorize, onlyForAdmin}
